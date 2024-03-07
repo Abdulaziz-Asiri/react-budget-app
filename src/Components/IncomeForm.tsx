468a96134e2bd37e952c7110f42073c0f88a0381
AZ-007 <aboodi-75-@hotmail.com>
2024-03-07 16:06:54 +0300

    Add functions to handle and submit the input

--- src/Components/IncomeForm.tsx
+++ src/Components/IncomeForm.tsx
@@ -1,24 +1,43 @@
 import { Button } from "./Button";
 
-export function Incomeform() {
-    
+export function Incomeform({
+  handleChangeSource,
+  handleChangeAmount,
+  handleChangeDate,
+  handleSubmit
+}) {
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="resource" id="resource">
           Income Soucrce
         </label>
-        <input type="text" name="resource" placeholder="Enter Your Income" />
+        <input
+          type="text"
+          name="resource"
+          placeholder="Enter Your Income"
+          onChange={handleChangeSource}
+        />
       </div>
       <div>
         <label htmlFor="amount" id="amount">
           Expenses
         </label>
-        <input type="text" name="amount" placeholder="Enter Your Expenses" />
+        <input
+          type="text"
+          name="amount"
+          placeholder="Enter Your Expenses"
+          onChange={handleChangeAmount}
+        />
       </div>
       <div>
         <label htmlFor="datename"> Date</label>
-        <input type="date" name="datename" id="dataname" />
+        <input
+          type="date"
+          name="datename"
+          id="dataname"
+          onChange={handleChangeDate}
+        />
       </div>
       <Button label="Add income" />
     </form>
